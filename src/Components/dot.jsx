0c508f86a955bfe5eb41f895e307b178141b3e95
yama-chiiii <nagayama0409@gmail.com>
2023-02-27 15:27:10 +0900

    Third commit

--- src/Components/dot.jsx
+++ src/Components/dot.jsx
@@ -9,51 +9,39 @@ const Dot = () => {
   const y = 20;//縦20マス
 
   const [colorIndex, setColorIndex] = useState("#ffffff");
-  const colorPallette = ['red', 'yellow', 'blue', 'gray', 'green']
+  const colorPallette = ['red', 'yellow', 'blue', 'gray', 'black']
   const [canvas, setCanvas] = useState(new Array(x * y).fill("white"));
 
+  useEffect(() => {
+    if(window.localStorage){
+      const json = localStorage.getItem("canvas");
+      setCanvas(JSON.parse(json));
+      
+    }
+  }, []);
   return (
     <div>
-      <Grid container spacing={0} columns={x}>
-        {
-          canvas.map((item, index) => {
-            return (
-              <Grid xs={1} key={index}>
-                <div onClick={() => {
-                  console.log(index);
-                  
-                }}
-                  
-                >
-
-                {/* <div onClick={() => {
-                  // setCanvas((prevcanvas) =>
-                  //     prevcanvas.map((previtem, previndex) => (index === previndex ? colorIndex : previtem))
-                  // );
-                  // console.log(canvas);
-                  console.log("test")
-                }}> */}
-                  <Box sx={{ margin: "1%", paddingTop: "100%", backgroundColor: item }}></Box>
-                </div>
-              </Grid>
-            )
-          })
-        }
-
-        {/* <Grid item xs={6} md={8}>
-                    <div style={{ backgroundColor: "green" }}>yahhoi</div>
+      <div style={{ width: "450px", height: "450px", marginTop: "50px", marginLeft: "100px", marginBottom: "40px" }}>
+        <Grid container spacing={0} columns={x}>
+          {
+            canvas.map((item, index) => {
+              return (
+                <Grid xs={1} key={index}>
+                  <div onClick={() => {
+                    setCanvas((prevcanvas) =>
+                      prevcanvas.map((previtem, previndex) => (index === previndex ? colorIndex : previtem))
+                    );
+                    // console.log(canvas);
+                    // console.log("test")
+                  }}>
+                    <Box sx={{ margin: "2%", paddingTop: "100%", backgroundColor: item }}></Box>
+                  </div>
                 </Grid>
-                <Grid item xs={6} md={4}>
-                    <div style={{ backgroundColor: "green" }}>wahhoi</div>
-                </Grid>
-                <Grid item xs={6} md={4}>
-                    <div style={{ backgroundColor: "green" }}>wa-i</div>
-                </Grid>
-                <Grid item xs={6} md={8}>
-                    <div style={{ backgroundColor: "green" }}>ya-i</div>
-                </Grid> */}
-
-      </Grid>
+              )
+            })
+          }
+        </Grid>
+      </div>
       {
 
         colorPallette.map((color, index) => {
@@ -67,88 +55,21 @@ const Dot = () => {
 
       }
       <p>{colorIndex}</p>
-    </div >
+      <button onClick={() => {
+        setCanvas((prevcanvas) =>
+          prevcanvas.map((previtem) => (previtem === "black" ? previtem : "white")))
+      }}>全部消す</button>
+
+      <button onClick={() => {
+        setCanvas((new Array(x * y).fill("white")))
+      }}>跡形もなく消す</button>
+
+      <button onClick={() => {
+        const json = JSON.stringify(canvas);
+        localStorage.setItem("canvas", json);      
+      }}>保存</button>
+    </div>
   )
-
-
-  // //useEffect分かんないんだっピ…
-  // //useEffect(() => {
-  // // const json = JSON.stringify(dotTbl)
-  // //localStorage.setItem("dotTbl",json)
-  // //},[dotTbl])
-
-  // var dotTbl = document.getElementById("dotTBL");
-  // var colTbl = document.getElementById("colorTBL");
-
-  // var canvas = document.getElementById("picture");
-  // var context = canvas.getContext("2d");
-
-  // //表の部分のコード
-  // function dotTable() {
-  //     for (var i = 0; i < x; i++) {
-  //         var row = dotTbl.insertRow(-1);
-  //         for (var j = 0; j < y; j++) {
-  //             var cell = row.insertCell(-1);
-  //             cell.onclick = function () {
-  //                 this.style.backgroundColor = Color_index;
-  //             }
-  //         }
-  //     }
-  // }
-
-  // //色を定義
-  // var COL_TBL = ["green", "gray", "red", "yellow", "blue"];
-  // function colTable() {
-  //     for (var j = 0; j < COL_TBL.length; j++) {
-  //         var cell = colTbl.rows[0].insertCell(-1);
-  //         cell.style.backgroundColor = COL_TBL[j];
-  //         cell.onclick = function () {
-  //             Color_index = this.style.backgroundColor;
-  //         }
-  //     }
-  // }
-
-
-  // function clearTable() {
-  //     for (var i = 0; i < x; i++) {
-  //         for (var j = 0; j < y; j++) {
-  //             if (dotTbl.rows[i].cells[j].style.backgroundColor !== "green") {
-  //                 dotTbl.rows[i].cells[j].style.backgroundColor = "white";
-  //             }
-  //         }
-  //     }
-  //     context.fillStyle = "white";
-  //     context.fillRect(0, 0, 20, 20);
-  // }
-
-  // function cnvCanvas() {
-  //     for (var i = 0; i < x; i++) {
-  //         for (var j = 0; j < y; j++) {
-  //             var canvas_Color = dotTbl.rows[i].cells[j].style.backgroundColor;
-  //             context.fillStyle = canvas_Color;
-  //             context.fillRect(j, i, 1, 1);
-  //         }
-  //     }
-  // }
-
-  // window.onload = function () {//ブラウザをローディングした時に実行
-  //     dotTable();
-  //     colTable();
-  //     clearTable();
-  //     cnvCanvas();
-  // }
-
-  // return (
-  //     <div>
-  //         <div>
-  //             <table id="dotTBL"></table>
-  //             <table id="colorTBL"><tr></tr></table>
-  //         </div>
-  //         <input type="button" class="clear" value="ぜんぶけす" onclick="clearTable()" />
-  //         <input type="button" class="canvas" value="キャンバス" onclick="cnvCanvas()" />
-  //         <canvas id="picture" width="20" height="20" style="border: solid 1px;"></canvas>
-  //     </div>
-  // )
 }
 
 
