8f403be13fa39dad0b011c90b4129433d0ec41e3
yama-chiiii <nagayama0409@gmail.com>
2023-02-28 13:59:44 +0900

    crossOrigin

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -19,6 +19,7 @@ const Canvas = () => {
         const auth = getAuth();
         getDownloadURL(ref(getStorage(), `canvaMental/${auth.currentUser.uid}`)).then((url) => {
             chara.src = url;
+            chara.crossOrigin = 'anonymous';
         }).catch((error) => {
             console.log(error);
             const localStorageImage = localStorage.getItem('imgBase64');
